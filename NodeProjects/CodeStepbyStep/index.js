acbf5cd918ac4b3ae3c2675233afc273808f452d
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2023-05-02 15:22:51 +0530

    commit

--- NodeProjects/CodeStepbyStep/index.js
+++ NodeProjects/CodeStepbyStep/index.js
@@ -1,15 +1,44 @@
-// const http = require('http');
+// // const http = require('http');
 
-// http.createServer((req,resp) =>{
+// // http.createServer((req,resp) =>{
 
-//     resp.write("hello sandeep k ");
+// //     resp.write("hello sandeep k ");
+// //     resp.end();
+// // }).listen(4600,()=>{console.log("servewr running")});
+
+
+// // const http =require('http')
+
+// // http.createServer((req,resp)=>{
+// //     resp.write("hii sandeep kadam")
+// //     resp.end()
+// // }).listen(5000,()=>{console.log("server running")})
+
+
+
+// // const http = require('http')
+
+// // http.createServer((req,resp)=>{
+// //     resp.write("hello sandeep")
+// //     resp.end()
+// // }).listen(5000,()=>{console.log("server running")})
+
+// const http =require('http')
+// const data=require('./data')
+// http.createServer((req,resp)=>{
+
+//     resp.writeHead(200,{'content-type':'application'});
+//     resp.write(JSON.stringify(data));
 //     resp.end();
-// }).listen(4600,()=>{console.log("servewr running")});
+// }).listen(5000)
 
 
-const http =require('http')
+// const fs =require('fs')
+// const path =require('path')
+// const dirpath=path.join(__dirname,'CRUD');
+// const filepath=`${dirpath}/apple.txt`;  
+// // const filepaths=`${dirpath}/pinapple.txt`;  
 
-http.createServer((req,resp)=>{
-    resp.write("hii sandeep kadam")
-    resp.end()
-}).listen(5000,()=>{console.log("server running")})
+// // fs.writeFileSync(filepath,'this is apple file');
+// // fs.writeFileSync(filepaths,'this is pinapple file');
+
